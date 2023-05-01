cfdb5c118809c78001f7a6595f567bcdb5a627f5
Alex <asgaroth@yandex.ru>
2023-05-01 20:13:21 +0300

    feat: generate keyboard in js, delete static html

--- app.js
+++ app.js
@@ -46,3 +46,97 @@ wrapper.appendChild(footer);
 
 // Add base markup
 bodyTag.appendChild(wrapper);
+
+class Keyboard {
+  constructor() {
+    this.keyIds = [
+      'Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace',
+      'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete',
+      'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter',
+      'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight',
+      'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight',
+    ];
+
+    this.enLower = [
+      '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '⌫',
+      'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del',
+      'Caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '↵',
+      '⇧', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑', '⇧',
+      'Ctrl', '⊞', 'Alt', ' ', 'Alt', '←', '↓', '→', 'Ctrl',
+    ];
+  }
+
+  buildKeyboard() {
+    let row;
+    let key;
+    for (let i = 0; i < this.keyIds.length; i += 1) {
+      if (i === 0 || i === 14 || i === 29 || i === 42 || i === 55) {
+        row = document.createElement('div');
+        row.className = 'keyboard__row';
+        keyboardContainer.appendChild(row);
+      }
+
+      key = document.createElement('div');
+      key.className = 'keyboard__row-key';
+      key.setAttribute('id', this.keyIds[i]);
+      key.innerText = this.enLower[i];
+
+      row.appendChild(key);
+
+      if (i === 13) {
+        key.className = 'keyboard__row-key backspace';
+      }
+
+      if (i === 14) {
+        key.className = 'keyboard__row-key tab';
+      }
+
+      if (i === 28) {
+        key.className = 'keyboard__row-key del';
+      }
+
+      if (i === 29) {
+        key.className = 'keyboard__row-key caps';
+      }
+
+      if (i === 41) {
+        key.className = 'keyboard__row-key enter';
+      }
+
+      if (i === 42) {
+        key.className = 'keyboard__row-key l-shift';
+      }
+
+      if (i === 53) {
+        key.className = 'keyboard__row-key arrow up';
+      }
+
+      if (i === 54) {
+        key.className = 'keyboard__row-key r-shift';
+      }
+
+      if (i === 55) {
+        key.className = 'keyboard__row-key l-ctrl';
+      }
+
+      if (i === 58) {
+        key.className = 'keyboard__row-key space';
+      }
+
+      if (i === 63) {
+        key.className = 'keyboard__row-key r-ctrl';
+      }
+    }
+  }
+}
+
+const keyboard = new Keyboard();
+
+keyboard.buildKeyboard();
+
+keyboardContainer.querySelector('#CapsLock').classList.add('test');
+
+// document.addEventListener('keydown', (event) => {
+//   // console.log(event.code);
+//   console.log(event.key);
+// });
